7c408d197e3084bf6e39be34061d3aaf002546e3
VolodumurGruv <gru.v@i.ua>
2021-06-16 23:00:01 +0300

    the basic template is ready

--- app.js
+++ app.js
@@ -5,22 +5,36 @@ const io = new Server(http);
 // what above we also can write as belowe
 // const io = require("socket.io")(http);
 
+const dataObj = {
+	candy: "it's a sweet product which was maden with sugar",
+	honey: "it's a sweet product which was maden by bees",
+	beeQueen: "18 days",
+	bee: "21 days",
+};
+
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/chat-bot.html");
 });
 
 io.on("connection", (socket) => {
 	console.log("user connected");
-
+	// console.log(socket);
 	socket.on("chat message", (msg) => {
 		//will send to everyone including the sender
 		io.emit("chat message", msg);
 	});
 
-	// send to everyone except for a certain emitting socket
-	// don't work don't know why
-	socket.broadcast.emit("hi");
+	socket.on("show", (msg) => {
+		for (let item in dataObj) {
+			io.emit("show", item);
+			if (msg === item) {
+				io.emit(item, dataObj[item]);
+			}
+		}
+	});
 
+	// send to everyone except for a certain emitting socket
+	socket.broadcast.emit("chat message", "user join");
 	// send event to everyone
 	// io.emit("chat message", {
 	// 	someProperty: "some property",
@@ -28,7 +42,11 @@ io.on("connection", (socket) => {
 	// });
 	socket.on("disconnect", () => {
 		console.log("user disconnected");
+		io.emit("chat message", "see you next time");
+		socket.broadcast.emit("chat message", "user left");
 	});
 });
+
+io.on("show", (socket) => {});
 
 http.listen(3000, () => console.log("Listening on port 3000"));
